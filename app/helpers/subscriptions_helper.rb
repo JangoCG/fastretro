096cac5f30ef640f7b763c5936d3d7d80e51585b
Cengiz Guertusgil <cengiz@cengizg.com>
2026-02-13 22:09:47 +0100

    subscription info

---
+++ app/helpers/subscriptions_helper.rb
@@ -0,0 +1,15 @@
+module SubscriptionsHelper
+  def subscription_period_end_action(subscription)
+    if subscription.to_be_canceled?
+      "Your Fast Retro subscription ends on"
+    elsif subscription.canceled?
+      "Your Fast Retro subscription ended on"
+    else
+      "Your next payment is <b>#{format_currency(subscription.next_amount_due)}</b> on".html_safe
+    end
+  end
+
+  def format_currency(amount)
+    number_to_currency(amount, precision: (amount % 1).zero? ? 0 : 2)
+  end
+end
---
+++ test/helpers/subscriptions_helper_test.rb
@@ -0,0 +1,24 @@
+require "test_helper"
+require "ostruct"
+
+class SubscriptionsHelperTest < ActionView::TestCase
+  include SubscriptionsHelper
+
+  test "subscription_period_end_action shows end date message for cancel at period end" do
+    subscription = OpenStruct.new(to_be_canceled?: true, canceled?: false, next_amount_due: 20)
+
+    assert_equal "Your Fast Retro subscription ends on", subscription_period_end_action(subscription)
+  end
+
+  test "subscription_period_end_action shows ended message for canceled subscription" do
+    subscription = OpenStruct.new(to_be_canceled?: false, canceled?: true, next_amount_due: 20)
+
+    assert_equal "Your Fast Retro subscription ended on", subscription_period_end_action(subscription)
+  end
+
+  test "subscription_period_end_action shows next payment amount for active subscription" do
+    subscription = OpenStruct.new(to_be_canceled?: false, canceled?: false, next_amount_due: 20)
+
+    assert_equal "Your next payment is <b>$20</b> on", subscription_period_end_action(subscription)
+  end
+end
