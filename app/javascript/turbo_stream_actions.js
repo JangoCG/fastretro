bfa27687be2f22a15ff7ca61034aa6f8763de4f0
Cengiz Guertusgil <cengiz@cengizg.com>
2026-02-14 01:08:03 +0100

    fix action items not shown

--- app/javascript/turbo_stream_actions.js
+++ app/javascript/turbo_stream_actions.js
@@ -2,11 +2,12 @@ import { Turbo } from "@hotwired/turbo-rails"
 
 // Custom Turbo Stream action to redirect all connected clients
 Turbo.StreamActions.redirect = function() {
-  console.log("[Turbo] Redirect action received", this)
   const url = this.getAttribute("url")
-  console.log("[Turbo] Redirecting to:", url)
   if (url) {
-    Turbo.visit(url, { action: "replace" })
+    const targetPath = new URL(url, window.location.origin).pathname
+    if (window.location.pathname !== targetPath) {
+      Turbo.visit(url, { action: "replace" })
+    }
   }
 }
 
